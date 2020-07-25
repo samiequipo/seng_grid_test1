3dcbb7d92d001edf14283bf6696f6aac1f198597
Samuel <samiequipo@gmail.com>
2020-07-25 14:16:42 -0300

    hidden user and password

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
-  :user_name            =>  'app178984735@heroku.com',
-  :password             =>  'w3auitjy0108',
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
   :domain               =>  'heroku.com',
   :enable_starttls_auto  =>  true
 }
